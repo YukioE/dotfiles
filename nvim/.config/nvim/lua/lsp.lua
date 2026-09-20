56c83d51cd8975164c0c6f85c237d6abdb411595
YukioE <93155932+YukioE@users.noreply.github.com>
2026-09-20 19:23:47 +0200

    remove null ls

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -3,17 +3,11 @@ vim.pack.add({
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
     { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
-    { src = "https://github.com/nvimtools/none-ls.nvim" },
 }, { confirm = false })
 
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-tool-installer").setup({})
-require("null-ls").setup({
-    sources = {
-        require("null-ls").builtins.formatting.prettierd,
-    },
-})
 
 vim.diagnostic.config({
     signs = {
